b1252a8425c04cfdbed127746ecb63e3173b5657
dimitar.ivanov <dimitarivanov523@gmail.com>
2022-03-09 13:20:42 +0200

    firzt

---
+++ src/main/java/edu/school/ElektronenBelejnik/ElektronenBelejnikApplication.java
@@ -0,0 +1,13 @@
+package edu.school.ElektronenBelejnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElektronenBelejnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElektronenBelejnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/school/ElektronenBelejnik/ElektronenBelejnikApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.school.ElektronenBelejnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElektronenBelejnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
